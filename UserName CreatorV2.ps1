cc4313866800fc2b80c1df51d60c85aa6eb412ab
puseyr <pusey.ronald@gmail.com>
2022-05-15 22:16:29 -0400

    Adding PS1 Script

---
+++ UserName CreatorV2.ps1
@@ -0,0 +1,34 @@
+﻿$firstName = Read-Host("First Name: ")
+$lastName = Read-Host("Last Name: ")
+$i = $lastName.Length
+$userName = " "
+
+if ($i -le 7){
+    
+    $userName = $lastName.Substring(0,$i).ToLower()+$firstName.Substring(0,1).ToLower()
+
+    }
+
+elseif ($i -ge 7){
+    
+    $userName = $lastName.substring(0,7).ToLower()+$firstName.Substring(0,1).ToLower()
+
+    $i++
+
+    }
+
+$userName | clip
+
+Write-Host("`nUsername Copied to clipboard!`n") -ForegroundColor Green
+
+Write-Host($userName) -ForegroundColor Cyan
+
+Read-Host -Prompt "Press any key to continue..."
+
+
+
+<#
+Future features:
+    - Check against AD if $userName exists already and make an alternate
+    - Create new user and populate with user info
+#>
